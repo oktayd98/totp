4f9f35439bdb089ba4816fef100027f11dcfc789
oktay <oktaydonmez98@gmail.com>
2023-08-12 11:18:29 +0300

    delete an otp by name

--- cmd/delete.go
+++ cmd/delete.go
@@ -5,7 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/oktayd98/totp/types"
+	"github.com/oktayd98/totp/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -15,10 +18,35 @@ var deleteCmd = &cobra.Command{
 	Short: "Deletes an otp record",
 	Long:  `Deletes an otp record by name.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		name, _ := cmd.Flags().GetString("name")
+
+		delete(name)
 	},
+}
+
+func delete(name string) {
+	filePath := utils.GetFilePath()
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Println("File could not find. You must run create command first.")
+		return
+	}
+
+	existingData := utils.ReadJSONFromFile(filePath)
+	var newData types.OTPData
+
+	for _, v := range existingData.OTPs {
+		if v.Name != name {
+			newData.OTPs = append(newData.OTPs, v)
+		}
+	}
+
+	fmt.Println(newData)
 }
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
+	deleteCmd.Flags().StringP("name", "n", "", "Name of OTP")
+
+	deleteCmd.MarkFlagRequired("name")
 }
