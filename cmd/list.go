fb757fbf60a559f5fb61dac80714d64c3a3647ff
oktay <oktaydonmez98@gmail.com>
2023-08-12 12:06:47 +0300

    list all otp records

--- cmd/list.go
+++ cmd/list.go
@@ -5,7 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/oktayd98/totp/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -15,10 +18,55 @@ var listCmd = &cobra.Command{
 	Short: "Lists all otp records.",
 	Long:  `Lists all names of saved otp records.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		showKeys, _ := cmd.Flags().GetBool("show-keys")
+		showOTPs, _ := cmd.Flags().GetBool("show-otps")
+
+		list(showKeys, showOTPs)
 	},
+}
+
+func list(showKeys bool, showOTPs bool) {
+	filePath := utils.GetFilePath()
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Println("File could not find. You must run create command first.")
+		return
+	}
+
+	data := utils.ReadJSONFromFile(filePath)
+
+	tableHeader := table.Row{"#", "Name"}
+	tableRows := []table.Row{}
+
+	for i, v := range data.OTPs {
+		tableRows = append(tableRows, table.Row{i + 1, v.Name})
+	}
+
+	if showOTPs {
+		tableHeader = append(tableHeader, "OTP")
+		for i, v := range data.OTPs {
+			otp := utils.GenerateOTP(v.Key)
+			tableRows[i] = append(tableRows[i], otp)
+		}
+	}
+
+	if showKeys {
+		tableHeader = append(tableHeader, "Key")
+		for i, v := range data.OTPs {
+			tableRows[i] = append(tableRows[i], v.Key)
+		}
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(tableHeader)
+	t.AppendRows(tableRows)
+	t.Render()
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolP("show-keys", "k", false, "Show secret keys")
+	listCmd.Flags().BoolP("show-otps", "o", true, "Show OTPs")
 }
